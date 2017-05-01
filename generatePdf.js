f3e0151976876c2ef4834ccbbf0ef44131874bf8
Sam <DevMac@Sams-MacBook-Pro.local>
2017-05-01 18:04:42 +0100

    updated demo

--- generatePdf.js
+++
@@ -1,8 +0,0 @@
-var htmlPdf = require('html-pdf');
-
-module.exports = (result, options, data) => { 
-  htmlPdf.create(data.html, options).toStream(function (err, stream) {
-      if(err) result(err);
-      return stream.pipe(result.stream);
-    }); 
-}
